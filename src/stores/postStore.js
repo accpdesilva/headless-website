ebf91b696299a62fd2b20c1fc3c833c6aeaf632d
Prasad De Silva <68392136+accpdesilva@users.noreply.github.com>
2025-06-21 14:53:03 +1000

    Integrated a vue component

--- src/stores/postStore.js
+++ src/stores/postStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { getPosts } from '../services/api';
+import axios from 'axios';
 
 export const usePostStore = defineStore('post', {
   state: () => ({
@@ -11,8 +11,16 @@ export const usePostStore = defineStore('post', {
       if (this.posts.length) return;
       this.loading = true;
       try {
-        const res = await getPosts();
-        this.posts = res.data;
+        const res = await axios.get('http://localhost/booster/wp-json/wp/v2/posts?_embed');
+        const rawPosts = res.data.slice(0, 3);
+
+        this.posts = rawPosts.map(post => {
+          const media = post._embedded?.['wp:featuredmedia']?.[0];
+          return {
+            ...post,
+            featured_media_url: media?.source_url || 'https://placehold.co/80x80',
+          };
+        });
       } catch (e) {
         console.error('Error fetching posts:', e);
       } finally {
