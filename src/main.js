4ec645dda5248471315990f27b7264a6e7af2d95
Prasad De Silva <68392136+accpdesilva@users.noreply.github.com>
2025-06-21 12:33:43 +1000

    Cleaning work
    Cleaning work

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
+import './assets/theme/sass/vendors/vendors.scss';
+import './assets/theme/css/icon.min.css';
+import './assets/theme/sass/icon/icon.scss';
 import './assets/theme/sass/style.scss';
+import './assets/theme/sass/responsive.scss';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
@@ -6,4 +10,23 @@ import App from './App.vue';
 
 const app = createApp(App);
 app.use(createPinia());
+
 app.mount('#app');
+
+// Load external scripts after app is mounted
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement('script');
+    s.src = src;
+    s.async = true;
+    s.onload = resolve;
+    s.onerror = reject;
+    document.body.appendChild(s);
+  });
+}
+
+ loadScript('/js/jquery.js')
+    .then(() => loadScript('/js/vendors/jquery.appear.js'))
+    .then(() => loadScript('/js/vendors/imagesloaded.pkgd.js'))
+    .then(() => loadScript('/js/main.js'))
+    .catch(err => console.error('Script load failed:', err));
